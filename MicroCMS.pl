e4fd2e20ddf1ac180fcc924a1353d8b6ecd98244
mccheung <mc_cheung@126.com>
2017-07-28 08:50:03 +0800

    disable google auth for default

--- MicroCMS.pl
+++ MicroCMS.pl
@@ -25,7 +25,7 @@ use ORLite {
         $dbh->do(
             'INSERT INTO user 
 				(email, password, token, rule) 
-				VALUES("admin@myproject.com","21232f297a57a5a743894a0e4a801fc3", "mfsg22lo","3");'
+				VALUES("admin@myproject.com","21232f297a57a5a743894a0e4a801fc3", "","3");'
         );
     },
 };
